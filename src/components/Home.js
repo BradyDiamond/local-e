9702927a8e41719026564a495939fc2eff099d7e
brady diamond <brady.diamond27@gmail.com>
2022-07-15 13:47:35 -0700

    add homepage

---
+++ src/components/Home.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import homeImage from "./../img/portland.jpg";
+import './Home.css';
+
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home-container'>
+        
+        <img className='home-image' src={homeImage}/>
+        
+        <div className='home-row'>
+
+        </div>
+
+        <div className='home-row'>
+        </div>
+
+        <div className='home-row'>
+
+        </div>
+
+  
+
+        <div className='home-row'>  
+        </div>
+
+
+
+
+        <div className='home-row'>
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
