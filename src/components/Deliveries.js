09623eeed06a305adfe54a6ba8bc9b7c2128be61
brady diamond <brady.diamond27@gmail.com>
2022-07-26 10:27:23 -0700

    set up deliveries

--- src/components/Deliveries.js
+++ src/components/Deliveries.js
@@ -1,12 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import './Deliveries.css';
+import { db } from "./firebase"
+import { useStateValue } from './StateProvider';
 
+function Deliveries() {
+  const [{cart, user}, dispatch] = useStateValue();
+  const [deliveries, setDeliveries] = useState([]);
 
-
+  useEffect(() => {
+    if(user) {
+    db.collection('users')
+      .doc(user?.uid)
+      .collection('orders')
+      .orderBy('created','desc')
+      .onSnapshot( snapshot => ( setDeliveries(snapshot.docs.map(doc => 
+      ({
+        id: doc.id,
+        data: doc.data()
+      })))
+      ))
+      } else {
+        setDeliveries([])
+      }
+      }, [user])
 
   return (
     <div className='deliveries'>
       <h1>Recent Deliveries</h1>
+      <div className='deliveries'>
+        {deliveries?.map(delivery => (
+          <Delivery  delivery={delivery} />
+        ))}
+      </div>
     </div>
   )
-
+}
 
 export default Deliveries
